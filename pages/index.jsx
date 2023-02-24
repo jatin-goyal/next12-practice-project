6d745a1e3c1eef6b40af634659a522d47fda26d7
Jatin Goyal <25JG2001@GMAIL.COM>
2023-02-24 16:30:56 +0530

    component navigation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("placing your order");
+    router.push("/product");
+  };
   return (
     <div>
       <h1>Home Page</h1>
+      <Link legacyBehavior href={"/blog"}>
+        <a>Go to blog page</a>
+      </Link>
+      <br />
+      <Link legacyBehavior href={"/product"}>
+        <a>Go to products page</a>
+      </Link>
+      <button onClick={handleClick}>Place order</button>
     </div>
   );
 }
--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
 export default function ProductList() {
   return (
     <div>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="/">
+        <h5>Go to home</h5>
+      </Link>
+      <h1>
+        <Link href="/product/1">
+          <h2>Product 1</h2>
+        </Link>
+      </h1>
+      <h1>
+        <Link href="/product/2">
+          <h2>Product 2</h2>
+        </Link>
+      </h1>
+      <h1>
+        <Link href="/product/3">
+          <h2>Product 3</h2>
+        </Link>
+      </h1>
     </div>
   );
 }
